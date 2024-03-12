49fcb67b211d7c574b5f34ea5cd23d709443c6ef
Kacper Zieliński <kacperzielinski.primary@gmail.com>
2024-03-12 17:06:04 +0100

    feat/insert new button into imageCard component

--- src/components/imageCard/index.jsx
+++ src/components/imageCard/index.jsx
@@ -1,20 +1,27 @@
-import Image from '../image'
+import Image from '../image';
 import Heading from '../heading';
 import Text from '../text';
-import './index.css'
+import './index.css';
+import Button from '../button';
 
-
-const ImageCard = ({image, heading, text, link}) => {
+const ImageCard = ({ image, heading, text, link }) => {
   return (
-    <div className="imageCard">
-      <Image src={image} alt={heading} className="imageCard__image"/>
+    <div className='imageCard'>
+      <Image src={image} alt={heading} className='imageCard__image' />
       <div className='imageCard__content'>
         <Heading level={3}>{heading}</Heading>
-        <Text className="imageCard__text"> {text} </Text>
-        <a href={link} className='imageCard__link'>Read More</a>
+        <Text className='imageCard__text'> {text} </Text>
+        <Button
+          ariaLabel='Read More'
+          className='imageCard__link'
+          href={link}
+          role='link'
+        >
+          Read More
+        </Button>
       </div>
     </div>
-  )
+  );
 };
 
 export default ImageCard;
