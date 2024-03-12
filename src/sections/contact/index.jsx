f0734635110e79d1811c3bfc457fc0cd223b799b
Kacper Zieliński <kacperzielinski.primary@gmail.com>
2024-03-12 16:13:08 +0100

    feat/add contact heading and text model and add css styles

--- src/sections/contact/index.jsx
+++ src/sections/contact/index.jsx
@@ -1,12 +1,23 @@
 import './index.css';
+import contactModel from '../../models/CONTACT_model';
+import Heading from '../../components/heading';
+import Text from '../../components/text';
 
 const Contact = () => {
-
   return (
-    <div className="contact">
-      :)
+    <div className='contact'>
+      <div className='container'>
+        <div className='content'>
+          <Heading level={2} color='black'>
+            {contactModel.heading}
+          </Heading>
+          <Text size='1.4rem' color='black'>
+            {contactModel.text}
+          </Text>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Contact;
